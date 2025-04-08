c6d47885a9a3671ee251142d32c83348829f5117
zzeen2 <jking120393@gmail.com>
2025-04-08 11:07:37 +0900

    [update] develop 브랜치에서 models 폴더 가져오기

--- models/locations.js
+++ models/locations.js
@@ -5,27 +5,27 @@
 const {DataTypes, Model} = require('sequelize');
 
 
-class ClubLocation  extends Model {
+class Location extends Model {
     static init(sequelize) {
         return super.init({
             club_id : {type : DataTypes.STRING(20),primaryKey : true, allowNull : false},
             point : {type : DataTypes.STRING(200), allowNull : false},
-            polygon : {type : DataTypes.STRING(200), allowNull : false}
+            poligon : {type : DataTypes.STRING(200), allowNull : false}
 
         }, {
             sequelize,
             timestamps : true ,
-            modelName : 'ClubLocation',
-            tableName : 'club_locations',
+            modelName : 'Location',
+            tableName : 'locations',
             charset : 'utf8mb4',
             collate : 'utf8mb4_general_ci'
         })
     }
     static associate(models) {
-        models.ClubLocation.belongsTo(models.Clubs, {foreignKey : 'club_id', target : 'club_id', onDelete : 'CASCADE'})
+        models.Locations.belongsTo(models.Clubs, {foreignKey : 'club_id_fk', target : 'club_id', onDelete : 'CASCADE'})
     }
 }
 
 
 
-module.exports = ClubLocation ;
+module.exports = Location;
